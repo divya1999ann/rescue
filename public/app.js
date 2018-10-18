c6a39dd396a4ca811e08e0a0dc56861fda0abd91
[vaskian] <namithasadeeshbabu@gmail.com>
2018-10-18 21:31:59 +0530

    adding cross

--- public/app.js
+++ public/app.js
@@ -5,13 +5,16 @@ function renderPerson(doc){
     let li = document.createElement('li');
     let Name = document.createElement('span');
     let Age = document.createElement('span');
+    let cross = document.createElement('div');
 
     li.setAttribute('data-id', doc.id);
     Name.textContent = doc.data().Name;
     Age.textContent = doc.data().Age;
+    cross.textContent = 'x';
 
     li.appendChild(Name);
     li.appendChild(Age);
+    li.appendChild(cross);
 
     personlist.appendChild(li);
 }
